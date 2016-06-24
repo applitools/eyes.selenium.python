990d52396f65ff0f3181fe2597f9cc07bcf53af9
danielputerman <danielputerman@users.noreply.github.com>
2016-06-24 14:22:52 +0300

    Updated viewport size algorithm.
    Now more efficient: it immediately tries to set the to the correct size, instead of starting from setting the browser window(!) size to the viewport(!) size.

--- applitools/_viewport_size.py
+++ applitools/_viewport_size.py
@@ -35,51 +35,49 @@ def get_viewport_size(driver):
     # noinspection PyBroadException
     try:
         width, height = driver.execute_script(_JS_GET_VIEWPORT_SIZE)
+        return {'width': width, 'height': height}
     except:
         logger.info('Failed to get viewport size. Only window size is available')
-        browser_size = driver.get_window_size()
-        width, height = browser_size['width'], browser_size['height']
-    return {'width': width, 'height': height}
-
-
-def _verify_size(verification_func, required_size, sleep_time=1, retries=3):
-    for retry in range(retries):
-        time.sleep(sleep_time)
-        current_size = verification_func()
-        if current_size['width'] == required_size['width'] \
-                and current_size['height'] == required_size['height']:
-            return True
-    return False
+        return driver.get_window_size()
 
 
 def set_viewport_size(driver, required_size):
+    _BROWSER_SIZE_CALCULATION_RETRIES = 2
+    _BROWSER_SET_SIZE_RETRIES = 3
+    _BROWSER_STABILIZATION_WAIT = 1  # Seconds
+    logger.debug("set_viewport_size({0})".format(required_size))
     if 'width' not in required_size or 'height' not in required_size:
         raise EyesError('Size must have width & height keys!')
-    logger.debug("set_viewport_size({0})".format(required_size))
-    # We move the window to (0,0) to have the best chance to be able to set the viewport size as requested.
-    driver.set_window_position(0, 0)
-    starting_size = required_size
-    driver.set_window_size(required_size['width'], required_size['height'])
-    if not _verify_size(driver.get_window_size, starting_size):
-        raise EyesError('Failed to set browser size!')
-    current_viewport_size = get_viewport_size(driver)
-    logger.debug("set_viewport_size(): initial viewport size: {0}".format(current_viewport_size))
-    current_browser_size = driver.get_window_size()
-    width_to_set = (2 * current_browser_size['width']) - current_viewport_size['width']
-    height_to_set = (2 * current_browser_size['height']) - current_viewport_size['height']
-    driver.set_window_size(width_to_set, height_to_set)
-    if not _verify_size(lambda: get_viewport_size(driver), required_size):
-        current_viewport_size = get_viewport_size(driver)
-        logger.debug("set_viewport_size(): viewport size: {0}".format(current_viewport_size))
-        logger.debug("set_viewport_size(): attempting one more time...")
-        current_browser_size = driver.get_window_size()
-        updated_width = current_browser_size['width'] + (required_size['width'] - current_viewport_size['width'])
-        updated_height = current_browser_size['height'] + (required_size['height'] - current_viewport_size['height'])
-        updated_browser_size = {'width': updated_width, 'height': updated_height}
-        logger.debug("set_viewport_size(): browser size: {0}".format(current_browser_size))
-        logger.debug("set_viewport_size(): required browser size: {0}".format(updated_browser_size))
-        driver.set_window_size(updated_width, updated_height)
-        if not _verify_size(lambda: get_viewport_size(driver), required_size):
-            current_viewport_size = get_viewport_size(driver)
-            logger.debug("set_viewport_size(): viewport size: {0}".format(current_viewport_size))
-            raise EyesError('Failed to set viewport size.')
+
+    actual_viewport_size = get_viewport_size(driver)
+    logger.debug("Current viewport size: {}".format(actual_viewport_size))
+    if actual_viewport_size == required_size:
+        return
+    # If the browser was initially maximized, we might need to repeat the process (border size for maximized browser is
+    # sometimes different than non-maximized).
+    for _ in range(_BROWSER_SIZE_CALCULATION_RETRIES):
+        # We move the window to (0,0) to have the best chance to be able to set the viewport size as requested.
+        driver.set_window_position(0, 0)
+        browser_size = driver.get_window_size()
+        logger.debug("Current browser size: {}".format(browser_size))
+        required_browser_size = {
+            'width': browser_size['width'] + (required_size['width'] - actual_viewport_size['width']),
+            'height': browser_size['height'] + (required_size['height'] - actual_viewport_size['height'])
+            }
+        logger.debug("Trying to set browser size to: {}".format(required_browser_size))
+        for retry in range(_BROWSER_SET_SIZE_RETRIES):
+            driver.set_window_size(required_browser_size['width'], required_browser_size['height'])
+            time.sleep(_BROWSER_STABILIZATION_WAIT)
+            browser_size = driver.get_window_size()
+            if browser_size == required_browser_size:
+                break
+            logger.debug("Current browser size: {}".format(browser_size))
+        else:
+            raise EyesError('Failed to set browser size!')
+
+        actual_viewport_size = get_viewport_size(driver)
+        logger.debug("Current viewport size: {}".format(actual_viewport_size))
+        if actual_viewport_size == required_size:
+            return
+    else:
+        raise EyesError('Failed to set the viewport size.')
