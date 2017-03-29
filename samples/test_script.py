e58aefc4dc98df983a38f439735ede0f1b0565ac
Daniel Puterman <daniel@dapt1.local>
2017-03-29 12:06:14 +0300

    Updated sample test.

--- samples/test_script.py
+++ samples/test_script.py
@@ -9,31 +9,23 @@ from applitools.common import StitchMode
 
 driver = webdriver.Chrome()
 
-logger.set_logger(StdoutLogger())
-eyes = Eyes('https://localhost.applitools.com')
+eyes = Eyes()
 eyes.api_key = os.environ['APPLITOOLS_API_KEY']
-# eyes.hide_scrollbars = True
-# For browser which only take screenshot of the viewport, you can uncomment the setting below, and
-# eyes will automatically create a full page screenshot.
 
+logger.set_logger(StdoutLogger())
+
+# Force Eyes to grab a full page screenshot.
 eyes.force_full_page_screenshot = True
 eyes.stitch_mode = StitchMode.CSS
 
 try:
     driver = eyes.open(driver, "Python app", "applitools", {'width': 800, 'height': 600})
     driver.get('http://www.applitools.com')
-    eyes.check_window("initial")
+    eyes.check_window("Home")
 
-    pricing_element = driver.find_element_by_css_selector("li.pricing a")
-    eyes.check_region_by_element(pricing_element, "pricing button")
+    automated_paragraph = driver.find_element_by_class_name("automated")
+    eyes.check_region_by_element(automated_paragraph, "Automated Testing Paragraph")
 
-    pricing_element.click()
-    eyes.check_window("pricing page")
-
-    driver.find_element_by_css_selector("li.contact-us a").click()
-    eyes.check_window("contact us page")
-    driver.find_element_by_class_name("input-name").send_keys("my name is what?")
-    eyes.check_window("name input")
     eyes.close()
 finally:
     driver.quit()
