45587afd05a30efe92ab2b5cd322125185cee0b0
Daniel Puterman <daniel.puterman@applitools.com>
2017-06-12 20:04:54 +0300

    Updated sample.

--- samples/test_script.py
+++ samples/test_script.py
@@ -1,11 +1,15 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 from applitools import logger
 from applitools.common import StitchMode
 from applitools.eyes import Eyes
+from applitools.geometry import Region
 from applitools.logger import StdoutLogger
 
-#os.environ['HTTPS_PROXY'] = "http://localhost:8888"
+# os.environ['HTTPS_PROXY'] = "http://localhost:9999"
+from applitools.target import Target, IgnoreRegionBySelector, FloatingRegion, FloatingBounds
 
 driver = webdriver.Chrome()
 
@@ -21,10 +25,10 @@ eyes.stitch_mode = StitchMode.CSS
 try:
     driver = eyes.open(driver, "Python app", "applitools", {'width': 800, 'height': 600})
     driver.get('http://www.applitools.com')
-    eyes.check_window("Home")
+    eyes.check_window("Home", target=Target().ignore(IgnoreRegionBySelector(By.CLASS_NAME, 'hero-container')).floating(FloatingRegion(Region(10, 20, 30, 40), FloatingBounds(10, 0, 20, 10))))
 
     hero = driver.find_element_by_class_name("hero-container")
-    eyes.check_region_by_element(hero, "Page Hero")
+    eyes.check_region_by_element(hero, "Page Hero", target=Target().ignore(Region(20, 20, 50, 50), Region(40, 40, 10, 20)))
 
     eyes.close()
 finally:
