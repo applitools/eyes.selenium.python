a1ca8e5c153439496c1742266b84c675fde609b2
addihorowitz <addihorowitz@gmail.com>
2017-07-10 13:09:15 +0300

    change code structure so it will be easy to import it

--- test/applitools_test.py
+++ test/applitools_test.py
@@ -6,28 +6,40 @@ from applitools.logger import StdoutLogger
 
 # os.environ['HTTPS_PROXY'] = "http://localhost:8888"
 
-driver = webdriver.Chrome()
+def create_driver():
 
-logger.set_logger(StdoutLogger())
-eyes = Eyes()
-eyes.api_key = os.environ['APPLITOOLS_API_KEY']
-eyes.force_full_page_screenshot = True
-eyes.hide_scrollbars = True
+    driver = webdriver.Chrome()
+    return driver
 
-try:
-    ## First test
-    driver = eyes.open(driver, "Python app", "Github Website", {'width': 800, 'height': 600})
-    driver.get('http://www.github.com')
-    eyes.check_window("initial")
-    results = eyes.close(False)
-    print(results)
+def run_test(driver):
+    logger.set_logger(StdoutLogger())
+    eyes = Eyes()
+    eyes.api_key = os.environ['APPLITOOLS_API_KEY']
+    eyes.force_full_page_screenshot = True
+    eyes.hide_scrollbars = True
 
-    ## Second test
-    driver = eyes.open(driver, "Python app", "Applitools Website", {'width': 900, 'height': 600})
-    driver.get('http://www.applitools.com')
-    eyes.check_window("initial")
-    results = eyes.close(False)
-    print(results)
-finally:
-    driver.quit()
-    eyes.abort_if_not_closed()
+    try:
+        ## First test
+        driver = eyes.open(driver, "Python app", "Github Website", {'width': 800, 'height': 600})
+        driver.get('http://www.github.com')
+        eyes.check_window("initial")
+        results = eyes.close(False)
+        print(results)
+
+        ## Second test
+        driver = eyes.open(driver, "Python app", "Applitools Website", {'width': 900, 'height': 600})
+        driver.get('http://www.applitools.com')
+        eyes.check_window("initial")
+        results = eyes.close(False)
+        print(results)
+    finally:
+        driver.quit()
+        eyes.abort_if_not_closed()
+
+def main():
+    driver = create_driver()
+    run_test(driver)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
