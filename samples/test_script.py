036def2de873b39f1f5e34a4cbcd2dc038f3b917
Daniel Puterman <daniel.puterman@applitools.com>
2017-12-12 11:39:56 +0200

    Updated check_image test sample.

--- samples/test_script.py
+++ samples/test_script.py
@@ -6,7 +6,7 @@ from applitools.geometry import Region
 from applitools.logger import StdoutLogger
 
 logger.set_logger(StdoutLogger())
-eyes = Eyes("https://localhost.applitools.com")
+eyes = Eyes()
 eyes.api_key = os.environ['APPLITOOLS_API_KEY']
 
 try:
@@ -14,7 +14,7 @@ try:
     with open("MY_IMAGE.png", 'rb') as f:
         image = f.read()
     eyes.check_image(image, None, "home")
-    eyes.check_image(image, Region(left=1773, top=372, width=180, height=220), "My region")
+    eyes.check_image(image, Region(left=330, top=140, width=510, height=380), "My region")
     eyes.close()
 finally:
     eyes.abort_if_not_closed()
